dc0be4a83e2ee4f98ed8d2abea74698596b2c208
Cody Liu <liu@codylab.com>
2017-02-23 09:43:05 +0900

    chore: add a README

---
+++ README.md
@@ -0,0 +1,5 @@
+# demo-viewanimation-sunset
+
+In this demo, I used [ObjectAnimator](https://developer.android.com/guide/topics/graphics/prop-animation.html#object-animator) which was introduced in Android 3.0 to create a animation of sunset and sunrise.
+
+![screen video](http://g.recordit.co/eQtBfzbC8y.gif)
--- app/src/main/java/com/codylab/sunset/SunsetFragment.java
+++ app/src/main/java/com/codylab/sunset/SunsetFragment.java
@@ -16,7 +16,6 @@ public class SunsetFragment extends Fragment {
 
     private View mSceneView;
     private View mSunView;
-    private View mSeaView;
     private View mSkyView;
     private int mBlueSkyColor;
     private int mSunsetSkyColor;
@@ -34,12 +33,16 @@ public class SunsetFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        Resources resources = getResources();
+        mBlueSkyColor = resources.getColor(R.color.blue_sky);
+        mSunsetSkyColor = resources.getColor(R.color.sunset_sky);
+        mNightSkyColor = resources.getColor(R.color.night_sky);
+
+
         View view = inflater.inflate(R.layout.fragment_sunset, container, false);
 
         mSceneView = view;
-
         mSunView = view.findViewById(R.id.sun);
-        mSeaView = view.findViewById(R.id.sea);
         mSkyView = view.findViewById(R.id.sky);
 
         mSceneView.setOnClickListener(new View.OnClickListener() {
@@ -49,10 +52,6 @@ public class SunsetFragment extends Fragment {
             }
         });
 
-        Resources resources = getResources();
-        mBlueSkyColor = resources.getColor(R.color.blue_sky);
-        mSunsetSkyColor = resources.getColor(R.color.sunset_sky);
-        mNightSkyColor = resources.getColor(R.color.night_sky);
 
         return view;
     }
@@ -79,7 +78,6 @@ public class SunsetFragment extends Fragment {
 
         AnimatorSet animationSet = new AnimatorSet();
         animationSet.play(heightAnimator).with(sunsetSkyAnimator).before(nightskyAnimator);
-//        animationSet.start();
         return animationSet;
     }
 
@@ -105,9 +103,9 @@ public class SunsetFragment extends Fragment {
 
         AnimatorSet animationSet = new AnimatorSet();
         animationSet.play(nightskyAnimator).before(heightAnimator).before(sunsetSkyAnimator);
-//        animationSet.start();
         return animationSet;
     }
+
     private void startSunsetAndSunrise(){
         AnimatorSet sunsetAnimation = getSunsetAnimation();
         AnimatorSet sunriseAnimation = getSunriseAnimation();
@@ -116,5 +114,4 @@ public class SunsetFragment extends Fragment {
         animationSet.playSequentially(sunsetAnimation, sunriseAnimation);
         animationSet.start();
     }
-
 }
